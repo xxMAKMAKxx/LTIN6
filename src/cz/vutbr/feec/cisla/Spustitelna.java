dd280bcd130f46540f735cc1038cee1aa627e7be
xxMAKMAKxx <red.mak.up@gmail.com>
2017-12-12 13:04:15 +0100

    Zacatek algoritmu

--- src/cz/vutbr/feec/cisla/Spustitelna.java
+++ src/cz/vutbr/feec/cisla/Spustitelna.java
@@ -1,20 +1,32 @@
 package cz.vutbr.feec.cisla;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Spustitelna {
 	public static void main(String args[]) {
 		final int LEFT = 0;
 		final int RIGHT = 1;
-		Dzbany d1 = new Dzbany();
-		Dzbany d2 = new Dzbany();
-		d2.fillPot(LEFT);
+		Dzbany dzbanUvod = new Dzbany();
+		dzbanUvod.setLeft(2);
 		
-		HashSet<Dzbany> set = new HashSet<Dzbany>();
-		set.add(d1);
-		set.add(d2);
+		HashSet<Dzbany> closed = new HashSet<Dzbany>();
+        LinkedList<Dzbany> open = new LinkedList<Dzbany>();
+        
+        //Pridani uvodniho dzbanu (0,0)
+        open.add(dzbanUvod);
+        
+        while(!open.isEmpty()) {
+        	Dzbany tmp = open.pop(); 
+        	//Je tmp resenim?
+        	if((tmp.getLeft() == 0 && tmp.getRight() == 2) || (tmp.getRight() == 0 && tmp.getLeft() == 2)) {
+        		System.out.println("Mam reseni!");
+        		tmp.printPots();
+        		System.exit(0);
+        	}
+        }
+        System.out.println("Reseni neexistuje!");
+				
 		
-		
-		System.out.println("Pocet prvku v mnozine je: "+set.size() + "\n"+ set);
 	}
 }
